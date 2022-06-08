39e3fe0befd391fe8ccd3810131605b835d96755
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2022-06-08 16:01:59 +0200

    dashboard transfer from rsemg

---
+++ dashboard/app.py
@@ -0,0 +1,6 @@
+import dash
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__,
+           external_stylesheets=external_stylesheets,
+           suppress_callback_exceptions=True)
