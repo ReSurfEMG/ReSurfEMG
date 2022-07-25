ef36a336167539e5361991656a3cc72c7cf0174c
Walter Baccinelli <w.baccinelli@esciencecenter.nl>
2022-07-25 09:49:34 +0200

    remove dashboard files

--- dashboard/app.py
+++
@@ -1,8 +0,0 @@
-from dash import Dash
-import dash_bootstrap_components as dbc
-
-external_stylesheets = [dbc.themes.BOOTSTRAP]
-app = Dash(__name__,
-           use_pages=True,
-           external_stylesheets=external_stylesheets,
-           suppress_callback_exceptions=True)
