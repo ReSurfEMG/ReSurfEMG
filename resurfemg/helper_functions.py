708dd30d60490d1e4377e24f7c8e295ddc7ac260
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2022-10-10 09:52:21 +0200

    update pre-ml pipeline

--- resurfemg/helper_functions.py
+++ resurfemg/helper_functions.py
@@ -11,7 +11,6 @@ from collections import namedtuple
 import math
 from math import log, e
 import copy
-# import builtins
 import builtins
 from scipy import signal
 from scipy.fft import fft, fftfreq
@@ -21,6 +20,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.decomposition import FastICA
 import textdistance
+from multi_lead_type import compute_ICA_two_comp_selective
 
 
 class Range(namedtuple('RangeBase', 'start,end')):
@@ -588,7 +588,7 @@ def working_pipeline_pre_ml(our_chosen_samples, picker='heart'):
     i.e. a three lead array into an EMG singal,
     the function is legacy code, and most
     processsing should be done with
-    :code:`working_pipeline_multi`
+    :code:`working_pipeline_pre_ml_multi`
 
     :param our_chosen_samples: the read EMG file arrays
     :type our_chosen_samples: ~numpy.ndarray
@@ -650,6 +650,68 @@ def slices_slider(array_sample, slice_len):
     """
     for i in range(len(array_sample) - slice_len + 1):
         yield array_sample[i:i + slice_len]
+
+
+
+def working_pipeline_pre_ml_multi(our_chosen_samples, our_chosen_leads, picker='heart'):
+    """
+    This is a pipeline to pre-process
+    an array of non-specific dimensions
+    e.g. a five lead array into an EMG singal,
+    of which we want leads 0 to 2 included
+
+    :param our_chosen_samples: the read EMG file arrays
+    :type our_chosen_samples: ~numpy.ndarray
+    :param our_chosen_leads: the read EMG file arrays that should be included
+    :type our_chosen_leads: tuple
+    :param picker: the picking strategy for independant components
+    :type picker: str
+
+    :returns: final_envelope_a
+    :rtype: ~numpy.ndarray
+    """
+
+
+    cut_file_data = bad_end_cutter_for_samples(
+        our_chosen_samples,
+        percent_to_cut=3,
+        tolerance_percent=5
+    )
+    bd_filtered_file_data = emg_bandpass_butter_sample(
+        cut_file_data,
+        5,
+        450,
+        2048,
+        output='sos'
+    )
+    # step for end-cutting again to get rid of filtering artifacts
+    re_cut_file_data = bad_end_cutter_for_samples(
+        bd_filtered_file_data,
+        percent_to_cut=3,
+        tolerance_percent=5
+    )
+    #  and do step for ICA
+    # TO BE REMOVED: components = compute_ICA_two_comp(re_cut_file_data)
+    components = compute_ICA_two_comp_selective(
+        re_cut_file_data,
+        use_all_leads=False,
+        desired_leads=our_chosen_leads,
+        )
+
+
+    #     the picking step!
+    if picker == 'peaks':
+        emg = pick_more_peaks_array(components)
+    elif picker == 'heart':
+        emg = pick_lowest_correlation_array(components, re_cut_file_data[0])
+    else:
+        emg = pick_lowest_correlation_array(components, re_cut_file_data[0])
+        print("Please choose an exising picker i.e. peaks or hearts ")
+    # now process it in final steps
+    abs_values = abs(emg)
+    final_envelope_d = emg_highpass_butter(abs_values, 150, 2048)
+
+    return final_envelope_d
 
 
 def slices_jump_slider(array_sample, slice_len, jump):
