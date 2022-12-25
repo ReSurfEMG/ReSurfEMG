5bfb3453c78244a20d0f99af84589c8032614ff3
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2022-12-25 16:51:42 +0100

    config testing

--- tests/test.py
+++ tests/test.py
@@ -8,7 +8,6 @@ import sys
 import numpy as np
 import scipy
 from tempfile import TemporaryDirectory
-from contextlib import contextmanager
 import json
 from unittest import TestCase, main
 
@@ -298,135 +297,37 @@ class TestArrayMath(unittest.TestCase):
         )
 
 
-# class TestConfig(TestCase):
+class TestConfig(TestCase):
 
-#     required_directories = {
-#         'root_emg_directory',
-#     }
-#     #required_directories = ['root_emg_directory']
+    required_directories = {
+        'root_emg_directory',
+    }
+    #required_directories = ['root_emg_directory']
 
-#     @contextmanager
-#     def cd(self, directory=None):
-#         if not directory:
-#             with TemporaryDirectory() as td:
-#                 old = os.getcwd()
-#                 os.chdir(td)
-#                 yield td
-#                 os.chdir(old)
-#         else:
-#             old = os.getcwd()
-#             os.chdir(td)
-#             yield td
-#             os.chdir(old)
+    # def test_roots_only(self):
+    #     with TemporaryDirectory() as td:
+    #         os.mkdir(os.path.join(td, 'root_emg_directory'))
+    #         raw_config = {
+    #             'root_emg_directory': os.path.join(td, 'root'),
+    #         }
+    #         config_file = os.path.join(td, 'config.json')
+    #         with open(config_file, 'w') as f:
+    #             json.dump(raw_config, f)
 
-#     def test_roots_only(self):
-#         with TemporaryDirectory() as td:
-#             os.mkdir(os.path.join(td, 'root_emg_directory'))
-#             raw_config = {
-#                 'root_emg_directory': os.path.join(td, 'root'),
-#             }
-#             config_file = os.path.join(td, 'config.json')
-#             with open(config_file, 'w') as f:
-#                 json.dump(raw_config, f)
+    #         # for root in self.required_directories:
+    #         #     os.mkdir(os.path.join(td, root))
 
-#             for root, dirs in self.required_directories.items():
-#                 for d in dirs:
-#                     os.mkdir(os.path.join(td, root, d))
+    #         config = Config(config_file)
+    #         assert config.get_directory('root_emg_directoy')
 
-#             config = Config(config_file)
-#             assert config.get_directory('root_emg_directoy')
+    def test_missing_config_path(self):
+        try:
+            Config('non existent')
+        except FileNotFoundError:
+            pass
+        else:
+            assert False, 'Didn\'t notify on missing config file'
 
-#     def test_data_only(self):
-#         with TemporaryDirectory() as td:
-#             raw_config = {
-#                 'data': 'data/',
-#                 'preprocessed': 'preprocessed/',
-#                 'metadata': 'metadata/',
-#                 'models': 'models/',
-#                 'data_2022': 'data-2022/',
-#                 'preprocessed_2022': 'preprocessed-2022/',
-#                 'metadata_2022': 'metadata-2022/',
-#                 'models_2022': 'models-2022/',
-#             }
-#             raw_config = {
-#                 k: os.path.join(td, v) for k, v in raw_config.items()
-#             }
-#             for v in raw_config.values():
-#                 os.mkdir(os.path.join(td, v))
-#             config_file = os.path.join(td, 'config.json')
-#             with open(config_file, 'w') as f:
-#                 json.dump(raw_config, f)
-#             config = Config(config_file)
-#             assert config.get_directory('data_2022')
-
-#     # def test_override_some(self):
-#     #     with TemporaryDirectory() as td:
-#     #         raw_config = {
-#     #             'root': 'root',
-#     #             'data': 'data/',
-#     #             'root_2022': 'root_2022',
-#     #             'data_2022': 'data-2022/',
-#     #         }
-#     #         raw_config = {
-#     #             k: os.path.join(td, v) for k, v in raw_config.items()
-#     #         }
-#     #         for v in raw_config.values():
-#     #             os.mkdir(os.path.join(td, v))
-#     #         os.mkdir(os.path.join(td, 'root', 'metadata'))
-#     #         os.mkdir(os.path.join(td, 'root_2022', 'metadata'))
-#     #         config_file = os.path.join(td, 'config.json')
-#     #         with open(config_file, 'w') as f:
-#     #             json.dump(raw_config, f)
-#     #         config = Config(config_file)
-#     #         assert config.get_directory('metadata_2022')
-
-#     def test_missing_config_path(self):
-#         try:
-#             Config('non existent')
-#         except Exception as e:
-#             assert 'Cannot find config files' in e.args[0]
-#         else:
-#             assert False, 'Didn\'t notify on missing config file'
-
-#     # def test_default_config_path(self):
-#     #     with self.cd() as td:
-#     #         with open(os.path.join(td, 'config.json'), 'w') as f:
-#     #             f.write('x')
-#     #         try:
-#     #             Config()
-#     #         except Exception as e:
-#     #             assert 'Found invalid JSON in ./config.json' == e.args[0]
-#     #         else:
-#     #             assert False, 'Didn\'t notify on missing config file'
-
-#     # def test_incorrect_data_path(self):
-#     #     with TemporaryDirectory() as td:
-#     #         os.mkdir(os.path.join(td, 'root'))
-#     #         os.mkdir(os.path.join(td, 'root_2022'))
-#     #         raw_config = {
-#     #             'root': 'root',
-#     #             'root_2022': 'root_2022',
-#     #         }
-#     #         raw_config = {
-#     #             k: os.path.join(td, v) for k, v in raw_config.items()
-#     #         }
-#     #         config_file = os.path.join(td, 'config.json')
-#     #         with open(config_file, 'w') as f:
-#     #             json.dump(raw_config, f)
-#     #         try:
-#     #             config = Config(config_file)
-#     #         except ValueError as e:
-#     #             expected_missing = []
-#     #             for root, dirs in self.required_directories.items():
-#     #                 for d in dirs:
-#     #                     full_path = os.path.join(raw_config[root], d)
-#     #                     if full_path not in e.args[0]:
-#     #                         expected_missing.append(d)
-#     #             assert not expected_missing, (
-#     #                 'Excpected to miss: {}'.format(missing)
-#     #             )
-#     #         else:
-#     #             assert False, 'Failed to identify missing data directory'
 
 if __name__ == '__main__':
     unittest.main()
