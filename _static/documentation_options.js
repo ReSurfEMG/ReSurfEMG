e50644ac2bd421e2da062c467ae9d7ff736cca58
drcandacemakedamoore <31033601+drcandacemakedamoore@users.noreply.github.com>
2023-02-10 08:41:15 +0000

    Deploying to gh-pages from @ ReSurfEMG/ReSurfEMG@80c51035d64f9d52f37d2de906b29e1e264176c3 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.6a',
+    VERSION: '0.0.7',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
