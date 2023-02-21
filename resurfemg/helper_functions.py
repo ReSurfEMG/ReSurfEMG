b20ca4b293458125d19f9de246d91215c5cb6f58
Candace Makeda Moore, MD <31033601+drcandacemakedamoore@users.noreply.github.com>
2023-02-21 14:52:45 +0100

    pull comment entirely

--- resurfemg/helper_functions.py
+++ resurfemg/helper_functions.py
@@ -29,15 +29,6 @@ import logging
 
 class Range(namedtuple('RangeBase', 'start,end')):
 
-    """Utility class for working with ranges (intervals).
-
-    # :ivar start: Start of the range
-    # :type start: ~number.Number
-    # :ivar end: End of the range
-    # :type end: ~number.Number
-
-
-    """
 
     def intersects(self, other):
         """Returns :code:`True` if this range intersects :code:`other` range.
