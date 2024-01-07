33718343cc9bdfad9326a224129fdb2007583a03
WarnaarRSP <r.s.p.warnaar@utwente.nl>
2024-01-07 17:15:42 +0100

    Notebook and functions for synthetic NMCdi data

--- resurfemg/config/config.py
+++ resurfemg/config/config.py
@@ -192,17 +192,104 @@ def make_synth_emg(long, max_abs_volt, humps):
     signal = synth_emg1*volt_multiplier
     return signal
 
-
-def simulate_emg_with_occlusions(t_start=0,
-                                 t_end=7*60,
-                                 emg_sample_rate=2048,   # hertz
-                                 rr=22,         # respiratory rate /min
-                                 ie_ratio=1/2,  # ratio btw insp + expir phase
-                                 tau_mus_up=0.3,
-                                 tau_mus_down=0.3,
-                                 occs_times_vals=[6*60+5, 6*60+21, 6*60+35]):
+def simulate_ventilator_with_occlusions(
+    occs_times_vals,     # Timing of occlusions (s)
+    t_start=0,
+    t_end=7*60,
+    fs_vent=2048,       # hertz
+    rr=22,              # respiratory rate /min
+    ie_ratio=1/2,       # Ratio between inspiratory and expiratory time
+    p_mus_max=5,        # Maximal respiratory muscle pressure (cmH2O)
+    tau_mus_up=0.3,     # Muscle contraction time constant
+    tau_mus_down=0.3,   # Muscle release time constant
+    c=.050,             # Respiratory system compliance (L/cmH2O)
+    r=5,                # Respiratory system resistance (cmH2O/L/s)
+    peep=5,             # Positive end-expiratory pressure (cmH2O)
+    dp=5                # Driving pressure above PEEP (cmH2O)
+):
     """
-    This function simulates an surface respiraotry emg with no ecg
+    This function simulates ventilator data with occlusion manuevers.
+    """
+    # Parse input parameters
+    ie_fraction = ie_ratio/(ie_ratio + 1)
+
+    # Time axis
+    t_vent = np.array(
+        [i/fs_vent for i in range(int(t_start*fs_vent), int(t_end*fs_vent))])
+
+    # Reference signal/Pattern generator
+    occs_times = np.array(occs_times_vals)
+
+    t_occs = np.floor(occs_times*rr/60)*60/rr
+    p_mus_block = (signal.square(t_vent*rr/60*2*np.pi + 0.1, ie_fraction)+1)/2
+    for i, t_occ in enumerate(t_occs):
+        i_occ = int(t_occ*fs_vent)    
+        p_mus_block[i_occ:i_occ+ int(fs_vent*60/rr)+1] = \
+            np.zeros((int(fs_vent *60/rr)+1,))   # Add occlusion manouevres
+
+    # Simulate up- and downslope dynamics of respiratory muscle pressure
+    pattern_gen_mus = np.zeros((len(t_vent),))
+    for i in range(1, len(t_vent)):
+        if (p_mus_block[i-1]-pattern_gen_mus[i-1]) > 0:
+            pattern_gen_mus[i] = (pattern_gen_mus[i-1] 
+                + (p_mus_block[i-1]-pattern_gen_mus[i-1])
+                / (tau_mus_up * fs_vent))
+        else:
+            pattern_gen_mus[i] = (pattern_gen_mus[i-1]
+                + (p_mus_block[i-1] - pattern_gen_mus[i-1])
+                / (tau_mus_down * fs_vent))
+
+    p_mus = p_mus_max * pattern_gen_mus
+
+    # Simulate up- and downslope dynamics of airway pressure
+    p_block = dp * (signal.square(t_vent*rr/60*2*np.pi, ie_fraction)+1)/2
+    tau_dp_up = 10
+    tau_dp_down = 5
+    p_dp = -p_mus
+    for i in range(1, len(t_vent)):
+        if np.any((((t_occs*fs_vent)-i)<=0)
+                  & ((((t_occs+60/rr)*fs_vent+1)-i)>0)):
+            # Occlusion pressure results into negative airway pressure:
+            p_dp[i] = p_dp[i-1]+(-p_block[i-1]-p_dp[i-1])/tau_dp_up
+        elif (p_block[i-1]-p_dp[i-1]) > 0:
+            p_dp[i] = p_dp[i-1]+(p_block[i-1]-p_dp[i-1])/tau_dp_up
+        else:
+            p_dp[i] = p_dp[i-1]+(p_block[i-1]-p_dp[i-1])/tau_dp_down
+
+    p_vent = peep + p_dp
+
+    # Calculate flows and volumes from equation of motion:
+    v_dot_vent = np.zeros((len(t_vent),))
+    v_vent = np.zeros((len(t_vent),))
+    for i in range(len(t_vent)-1):
+        if np.any((((t_occs*fs_vent)-i)<=0) 
+                  &  ((((t_occs+60/rr)*fs_vent+1)-i)>0)):
+            # During occlusion manoeuvre: flow and volume are zero
+            v_dot_vent[i+1] = 0
+            v_vent[i+1] = 0
+        else:
+            v_dot_vent[i+1] = ((p_dp[i] + p_mus[i]) - v_vent[i] / c)/r
+            v_vent[i+1] = v_vent[i] + v_dot_vent[i+1] * 1/fs_vent
+
+    y_vent = np.vstack((p_vent, v_dot_vent, v_vent))
+    return y_vent
+
+def simulate_emg_with_occlusions(
+    occs_times_vals,
+    t_start=0,
+    t_end=7*60,
+    emg_sample_rate=2048,   # hertz
+    rr=22,         # respiratory rate /min
+    ie_ratio=1/2,  # ratio between inspiratory and expiratory time
+    tau_mus_up=0.3,
+    tau_mus_down=0.3,
+    
+    emg_amp=5,     # Approximate EMG-RMS amplitude (uV)
+    drift_amp=100, # Approximate drift RMS amplitude (uV)
+    noise_amp=2    # Approximate baseline noise RMS amplitude (uV)
+):
+    """
+    This function simulates an surface respiratory emg with no ecg
     component but with occlusion manuevers.
     An ECG component can be added and mixed in later.
     """
@@ -215,45 +302,43 @@ def simulate_emg_with_occlusions(t_start=0,
             printable2 = 'should be at least a full resp. cycle from t_end'
             print(printable1 + printable2)
     # time axis
-    esr = emg_sample_rate
-    y_emg = np.array(
-        [i/esr for i in range(int(t_start*esr), int(t_end*esr))]
+    fs_emg = emg_sample_rate
+    t_emg = np.array(
+        [i/fs_emg for i in range(int(t_start*fs_emg), int(t_end*fs_emg))]
     )
 
     # reference signal pattern generator
-    emg_block = (signal.square(y_emg*rr/60*2*np.pi + 0.5, ie_fraction)+1)/2
+    emg_block = (signal.square(t_emg*rr/60*2*np.pi + 0.5, ie_fraction)+1)/2
     for i, t_occ in enumerate(t_occs):
-        esr = emg_sample_rate
-        n_occ = int(t_occ*emg_sample_rate)
-        blocker = np.arange(int(esr*60/rr)+1)/esr*rr/60*2*np.pi
+        i_occ = int(t_occ*emg_sample_rate)
+        blocker = np.arange(int(fs_emg*60/rr)+1)/fs_emg*rr/60*2*np.pi
         squared_wave = (signal.square(blocker, ie_fraction)+1)/2
-        emg_block[n_occ:n_occ+int(esr*60/rr)+1] = squared_wave
+        emg_block[i_occ:i_occ+int(fs_emg*60/rr)+1] = squared_wave
 
     # simulate up- and downslope dynamics of EMG
-    pattern_gen_emg = np.zeros((len(y_emg),))
+    pattern_gen_emg = np.zeros((len(t_emg),))
 
-    for i in range(1, len(y_emg)):
+    for i in range(1, len(t_emg)):
         pat = pattern_gen_emg[i-1]
-        esr = emg_sample_rate
+        fs_emg = emg_sample_rate
         if (emg_block[i-1]-pat) > 0:
-            pattern_gen_emg[i] = pat + (emg_block[i-1]-pat)/(tau_mus_up*esr)
+            pattern_gen_emg[i] = pat + (emg_block[i-1]-pat)/(tau_mus_up*fs_emg)
         else:
-            pattern_gen_emg[i] = pat + (emg_block[i-1]-pat)/(tau_mus_down*esr)
+            pattern_gen_emg[i] = pat + (emg_block[i-1]-pat)/(tau_mus_down*fs_emg)
 
     # make respiratory EMG component
-    part_emg = pattern_gen_emg * np.random.normal(0, 0.5, size=(len(y_emg), ))
+    part_emg = pattern_gen_emg * np.random.normal(0, 0.5, size=(len(t_emg), ))
 
     # make noise and drift components
-    part_noise = np.random.normal(0, 0.5, size=(len(y_emg), ))
-    part_drift = np.zeros((len(y_emg),))
+    part_noise = np.random.normal(0, 0.5, size=(len(t_emg), ))
+    part_drift = np.zeros((len(t_emg),))
 
     # mix channels, could be remixed with an ecg
-    x_emg = np.zeros((3, len(y_emg)))
-    x_emg[0, :] = 0.05 * part_emg + 1 * part_noise + 20 * part_drift
-    x_emg[1, :] = 4 * part_emg + 1 * part_noise + 20 * part_drift
-    x_emg[2, :] = 8 * part_emg + 1 * part_noise + 20 * part_drift
-    data_emg_samples = x_emg
-    return data_emg_samples
+    y_emg = np.zeros(t_emg.shape)
+    y_emg[:] = (emg_amp * part_emg + drift_amp * part_drift
+                   + noise_amp * part_noise)
+
+    return y_emg
 
 
 def make_realistic_syn_emg(loaded_ecg, number):
@@ -285,12 +370,12 @@ def make_realistic_syn_emg(loaded_ecg, number):
         emg_stack = np.vstack((emg_stack, emg3))
         heart_line = random.randint(0, 9)
         one_line_ecg = loaded_ecg[heart_line]
-        x_emg = np.zeros((3, emg_stack.shape[1]))
+        t_emg = np.zeros((3, emg_stack.shape[1]))
         ecg_out = np.array(200*one_line_ecg, dtype='float64')
-        x_emg[0] = ecg_out + np.array(0.05 * emg_stack[0], dtype='float64')
-        x_emg[1] = ecg_out + np.array(4 * emg_stack[1], dtype='float64')
-        x_emg[2] = ecg_out + np.array(8 * emg_stack[2], dtype='float64')
-        list_emg.append(x_emg)
+        t_emg[0] = ecg_out + np.array(0.05 * emg_stack[0], dtype='float64')
+        t_emg[1] = ecg_out + np.array(4 * emg_stack[1], dtype='float64')
+        t_emg[2] = ecg_out + np.array(8 * emg_stack[2], dtype='float64')
+        list_emg.append(t_emg)
     return list_emg
 
 
