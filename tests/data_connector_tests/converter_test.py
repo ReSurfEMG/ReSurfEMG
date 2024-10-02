368ea8fb6975e9b6f5c40a9174c7839133937953
rspwarnaarUT <r.s.p.warnaar@utwente.nl>
2024-10-02 17:48:34 +0200

    Implement standard load_file function

--- tests/data_connector_tests/converter_test.py
+++ tests/data_connector_tests/converter_test.py
@@ -7,34 +7,67 @@ from tempfile import TemporaryDirectory
 # tmsisdk_lite
 from resurfemg.data_connector.tmsisdk_lite import Poly5Reader
 # converter_functions 
+from resurfemg.data_connector import converter_functions
 from resurfemg.data_connector.converter_functions import poly5unpad
 from resurfemg.helper_functions.config import hash_it_up_right_all
 
-sample_emg = os.path.join(
+sample_path = os.path.join(
     os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(
         __file__)))),
-    'test_data',
+    'test_data')
+sample_emg_poly5 = os.path.join(
+    sample_path, 
     'emg_data_synth_quiet_breathing.Poly5',
 )
+sample_emg_mat = os.path.join(
+    sample_path, 
+    'emg_data_synth_quiet_breathing.mat',
+)
+sample_emg_csv = os.path.join(
+    sample_path, 
+    'emg_data_synth_quiet_breathing.csv',
+)
+
+
+class TestLoadData(unittest.TestCase):
+    def test_load_poly5(self):
+        np_array, *_ = converter_functions.load_file(
+            file_path=sample_emg_poly5
+        )
+        self.assertEqual(np_array.shape[0], 2)
+    
+    def test_load_mat(self):
+        np_array, *_ = converter_functions.load_file(
+            file_path=sample_emg_mat,
+            key_name='mat5_data',
+        )
+        self.assertEqual(np_array.shape[0], 2)
+
+    def test_load_csv(self):
+        np_array, *_ = converter_functions.load_file(
+            file_path=sample_emg_csv
+        )
+        self.assertEqual(np_array.shape[0], 2)
+
 
 class TestConverterMethods(unittest.TestCase):
 
     def test_poly5unpad(self):
-        reading =Poly5Reader(sample_emg)
-        unpadded= poly5unpad(sample_emg)
+        reading =Poly5Reader(sample_emg_poly5)
+        unpadded= poly5unpad(sample_emg_poly5)
         unpadded_line = unpadded[0]
         self.assertEqual(len(unpadded_line), reading.num_samples)
 
     def Poly5Reader(self):
-        reading =Poly5Reader(sample_emg)
+        reading =Poly5Reader(sample_emg_poly5)
         self.assertEqual(reading.num_channels, 3)
 
 
 class TestHashMethods(unittest.TestCase):
 
     def test_hash_it_up_right_all(self):
-        tempfile1 = 'sample_emg_t.Poly5'
-        tempfile2 = 'sample_emg_t.Poly5'
+        tempfile1 = 'sample_emg_poly5_t.Poly5'
+        tempfile2 = 'sample_emg_poly5_t.Poly5'
         with TemporaryDirectory() as td:
             with open(os.path.join(td, tempfile1), 'w') as tf:
                 tf.write('string')
