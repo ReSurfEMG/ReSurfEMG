d78f6dc5a0d5b69e8a33d9693439d06405cb2e95
rspwarnaarUT <r.s.p.warnaar@utwente.nl>
2024-11-28 16:52:11 +0100

    Update init files for optimized import functionality

--- resurfemg/__init__.py
+++ resurfemg/__init__.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
 from resurfemg.__version__ import __version__
+
+# Import the subpackages
+from . import cli
+from . import data_connector
+from . import helper_functions
+from . import pipelines
+from . import postprocessing
+from . import preprocessing
